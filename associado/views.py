06f122ffc423b0e40118388905597a93ba814c9c
unknown <monicakarolbraga@gmail.com>
2026-03-10 17:47:26 -0300

    salvando a segunda sessão do guia

--- associado/views.py
+++ associado/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
    return render(request,'associado/index.html')
+
+def perfil(request):
+   return render(request, 'associado/perfil.html')
